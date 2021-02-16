ce8b9f1b2b81b34fd3f0091188959aeed39ac1bb
山形　由佳 <uu164201@CA6595.local>
2021-02-16 12:13:35 +0900

    iconを追加

--- App.tsx
+++ App.tsx
@@ -3,15 +3,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { AddTodo } from './src/app/components/AddTodo';
 import { TodoList } from './src/app/components/TodoList';
+import { AntDesign, Feather } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
+
+const AddTodoIcon: React.FC<{color: string, size: number }> = ({ color, size }) => (
+  <AntDesign name="pluscircleo" size={size} color={color} />
+)
+
+const TodoListIcon: React.FC<{color: string, size: number }> = ({ color, size }) => (
+  <Feather name="list" color={color} size={size} />
+)
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={AddTodo} />
-        <Tab.Screen name="Settings" component={TodoList} />
+        <Tab.Screen
+        name="タスクを作成"
+        component={AddTodo}
+        options={{
+          tabBarIcon: AddTodoIcon
+        }}/>
+        <Tab.Screen
+        name="タスクを表示"
+        component={TodoList}
+        options={{
+          tabBarIcon: TodoListIcon
+        }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
