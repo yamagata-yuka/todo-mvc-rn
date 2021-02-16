8a2c422a55a36d32793a442599d320ec0e388273
山形　由佳 <uu164201@CA6595.local>
2021-02-16 13:30:47 +0900

    Stackをいれてheader追加

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 import { AddTodo } from './src/app/components/AddTodo';
 import { TodoList } from './src/app/components/TodoList';
 import { AntDesign, Feather } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
+
+const AddTodoStack = createStackNavigator();
+const TodoListStack = createStackNavigator();
+
+// 画面を追加するならこのreturnの中に書く
+// 画面間の遷移で移動したりできるようになる
+const AddTodoStackComponent = () => {
+  return (
+    <AddTodoStack.Navigator>
+      <AddTodoStack.Screen
+      name="タスクの追加"
+      component={AddTodo}
+      />
+    </AddTodoStack.Navigator>
+  )
+}
+
+const TodoListStackComponent = () => {
+  return (
+    <TodoListStack.Navigator>
+      <TodoListStack.Screen
+      name="タスク一覧"
+      component={TodoList}
+      />
+    </TodoListStack.Navigator>
+  )
+}
 
 const AddTodoIcon: React.FC<{color: string, size: number }> = ({ color, size }) => (
   <AntDesign name="pluscircleo" size={size} color={color} />
@@ -20,14 +48,14 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen
-        name="タスクを作成"
-        component={AddTodo}
+        name="AddTodo"
+        component={AddTodoStackComponent}
         options={{
           tabBarIcon: AddTodoIcon
         }}/>
         <Tab.Screen
-        name="タスクを表示"
-        component={TodoList}
+        name="TodoList"
+        component={TodoListStackComponent}
         options={{
           tabBarIcon: TodoListIcon
         }} />
